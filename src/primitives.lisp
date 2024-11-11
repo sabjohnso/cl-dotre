3a348faa99709803bba9f55e04e6d4c638878e09
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-11-11 15:49:26 -0600

    :art: [primitives] Added functions for constructing primitives

--- src/primitives.lisp
+++ src/primitives.lisp
@@ -2,37 +2,42 @@
 
 (defpackage :dotre.primitives
   (:nicknames :prim)
-  (:use :cl :dotre.character-class)
-  (:export #:item #:make-item
-           #:repeat #:make-repeat
-           #:alt #:make-alt
-           #:cut #:make-cut
-           #:seq #:make-seq
-           #:guard #:make-guard
-           #:run-lexeme))
+  (:shadow #:copy-seq)
+  (:use :cl :dotre.character-range :dotre.character-class )
+  (:export #:item
+           #:repeat
+           #:alt
+           #:cut
+           #:seq
+           #:guard
+           #:run-pattern))
 
 (in-package :dotre.primitives)
 
 (defstruct lexeme)
 
+(defstruct (item (:include lexeme)))
+
 (defstruct (guard (:include lexeme))
   (lexeme (make-lexeme) :type lexeme)
   (class (make-character-class) :type character-class))
 
-(defstruct (item (:include lexeme)))
-
 (defstruct (repeat (:include lexeme))
-  (lexeme (make-lexeme) :type lexeme)
+  (lexeme (make-item) :type lexeme)
   (lower nil :type (or null unsigned-byte))
   (upper nil :type (or null unsigned-byte)))
 
 (defstruct (alt (:include lexeme))
-  (first (make-lexeme) :type lexeme)
-  (second (make-lexeme) :type lexeme))
+  (first (make-item) :type lexeme)
+  (second (make-item) :type lexeme))
 
 (defstruct (cut (:include lexeme))
-  (first (make-lexeme) :type lexeme)
-  (second (make-lexeme) :type lexeme))
+  (first (make-item) :type lexeme)
+  (second (make-item) :type lexeme))
+
+(defstruct (seq (:include lexeme))
+  (first (make-item) :type lexeme)
+  (second (make-item) :type lexeme))
 
 
 (declaim (ftype (function (lexeme stream) unsigned-byte) primitive-lexeme-run))
@@ -70,23 +75,23 @@
          (unread-chars ,var ,inp)
          ,result))))
 
-(defun run-lexeme (lexeme inp)
+(defun run-pattern (lexeme inp)
   (typecase lexeme
     (guard
      (with-slots (lexeme class) lexeme
        (let ((char (peek-char nil inp nil)))
-         (if (and char (class-member (char-code char) class)) (run-lexeme lexeme inp) 0))))
+         (if (and char (class-member (char-code char) class)) (run-pattern lexeme inp) 0))))
     (item
      (if (peek-char nil inp nil) 1  0))
     (alt
      (with-slots (first second) lexeme
-       (max (run-lexeme first inp)
-            (run-lexeme second inp))))
+       (max (run-pattern first inp)
+            (run-pattern second inp))))
     (repeat
      (with-slots (lexeme lower upper) lexeme
        (labels ((recur (repetition char-count)
                   (if (and upper (>= repetition upper)) char-count
-                      (let ((n (run-lexeme lexeme inp)))
+                      (let ((n (run-pattern lexeme inp)))
                         (if (zerop n)
                             (if (or (null lower) (and lower (<= lower repetition))) char-count 0)
                             (with-chars-held (n inp)
@@ -94,13 +99,86 @@
          (recur 0 0))))
     (cut
      (with-slots (first second) lexeme
-       (let ((n (run-lexeme first inp)))
-         (if (zerop n) (run-lexeme second inp) n))))
+       (let ((n (run-pattern first inp)))
+         (if (zerop n) (run-pattern second inp) n))))
 
     (seq
      (with-slots (first second) lexeme
-       (let ((m (run-lexeme first inp)))
+       (let ((m (run-pattern first inp)))
          (if (zerop m) 0
              (with-chars-held (m inp)
-               (let ((n (run-lexeme second inp)))
+               (let ((n (run-pattern second inp)))
                  (if (zerop n) 0 (+ m n))))))))))
+
+
+(declaim (ftype (function () guard) item))
+(defun item ()
+  "Return an item that matches any character"
+  (make-guard
+   :lexeme (make-item)
+   :class (make-character-class (range 0 char-code-limit))))
+
+(deftype optional-unsigned-byte ()
+  `(or null unsigned-byte))
+
+(declaim
+ (ftype
+  (function (guard &key (:lower optional-unsigned-byte) (:upper optional-unsigned-byte)) guard)
+  repeat))
+
+(defun repeat (lexeme &key lower upper)
+  "Return a pattern matching a number of repetitions of `LEXEME'.
+
+The number is determined by `LOWER' and `UPPER'. If `LOWER'
+specifies the inclusive minimum number of matches, and if `LOWER' is
+`NIL' the minimum number of matches is 0. `UPPER' specifies the
+inclusive maximum number of matches, and if `UPPER' is `NIL', the
+number is unbounded."
+  (make-guard
+   :lexeme (make-repeat :lexeme lexeme :lower lower :upper upper)
+   ;; Note: The guard is used even if `LOWER' is nil or 0, because
+   ;; matching 0 is equivalent to failing.
+   :class (guard-class lexeme)))
+
+
+(declaim (ftype (function (guard character-class) guard) guard))
+
+(defun guard (lexeme class)
+  "Return a pattern with an additional constraint in character class."
+  (make-guard
+   :lexeme (guard-lexeme lexeme)
+   :class (class-intersection (guard-class lexeme) class)))
+
+(declaim (ftype (function (guard &rest guard) guard) alt cut seq))
+
+(defun alt (lexeme &rest lexemes)
+  "Return a pattern that matches any of the input patterns."
+  (if (null lexemes) lexeme
+      (let* ((second (apply #'alt lexemes))
+             (class (class-union (guard-class lexeme) (guard-class second))))
+        (make-guard
+         :lexeme (make-alt
+                  :first lexeme
+                  :second second)
+         :class class))))
+
+(defun cut (lexeme &rest lexemes)
+  "Return a pattern that matches when `LEXEMES' matches, or if it fails, when
+the pattern formed from `LEXEMES' matches."
+  (if (null lexemes) lexeme
+      (let ((lexemes (apply #'cut lexemes)))
+        (make-guard
+         :lexeme (make-cut
+                  :first lexeme
+                  :second lexemes)
+         :class (class-union (guard-class lexeme) (guard-class lexemes))))))
+
+(defun seq (lexeme &rest lexemes)
+  "Return a pattern that matches when each of the input patterns match
+sequentially."
+  (if (null lexemes) lexeme
+      (make-guard
+       :lexeme (make-seq
+                :first (guard-lexeme lexeme)
+                :second (apply #'seq lexemes))
+       :class (guard-class lexeme))))
--- test/primitives-test.lisp
+++ test/primitives-test.lisp
@@ -15,80 +15,57 @@
 
 (test item
   (with-input-from-string (inp "abc123")
-    (is (= 1 (run-lexeme (make-item) inp)))
+    (is (= 1 (run-pattern (item) inp)))
     (is (char= #\a (peek-char nil inp nil))))
   (with-input-from-string (inp "")
-    (is (= 0 (run-lexeme (make-item) inp)))))
+    (is (= 0 (run-pattern (item) inp)))))
 
 (test guard
   (with-input-from-string (inp "abc123")
-    (is (= 1 (run-lexeme
-              (make-guard
-               :lexeme (make-item)
-               :class (make-character-class
-                       (char-range #\a #\a)))
-              inp)))
+
+    (is (= 1 (run-pattern (guard (item) (make-character-class (char-range #\a #\a))) inp)))
     (is (char= #\a (peek-char nil inp nil)))
-    (is (= 0 (run-lexeme
-              (make-guard
-               :lexeme (make-item)
-               :class (make-character-class
-                       (char-range #\b #\b)))
-              inp)))
-    (is (char= #\a (peek-char nil inp nil)))))
+
+    (is (= 0 (run-pattern (guard (item) (make-character-class (char-range #\b #\b))) inp)))
+    (is (char= #\a (peek-char nil inp nil))))
+
+    (with-input-from-string (inp "")
+      (is (= 0 (run-pattern (guard (item) (make-character-class (char-range #\b #\b))) inp)))))
 
 (test repeat
   (with-input-from-string (inp "abc123")
-    (is (= 6 (run-lexeme (make-repeat :lexeme (make-item)) inp)))
+    (is (= 6 (run-pattern (repeat (item)) inp)))
     (is (char= #\a (peek-char nil inp nil)))
 
-    (is (= 3 (run-lexeme
-              (make-repeat
-               :lexeme (make-guard
-                        :lexeme (make-item)
-                        :class (make-character-class
-                                (char-range #\a #\z))))
+    (is (= 3 (run-pattern
+              (repeat (guard (item) (make-character-class (char-range #\a #\z))))
               inp)))
     (is (char= #\a (peek-char nil inp nil)))
 
-    (is (= 0 (run-lexeme
-              (make-repeat
-               :lexeme (make-guard
-                        :lexeme (make-item)
-                        :class (make-character-class
-                                (char-range #\0 #\9))))
+    (is (= 0 (run-pattern
+              (repeat (guard (item) (make-character-class (char-range #\0 #\9))))
               inp)))
     (is (char= #\a (peek-char nil inp nil)))
 
 
-    (is (= 2 (run-lexeme
-              (make-repeat
-               :lexeme (make-guard
-                        :lexeme (make-item)
-                        :class (make-character-class
-                                (char-range #\a #\z)))
+    (is (= 2 (run-pattern
+              (repeat
+               (guard (item)  (make-character-class (char-range #\a #\z)))
                :lower 2
                :upper 2)
               inp)))
     (is (char= #\a (peek-char nil inp nil)))
 
-    (is (= 3 (run-lexeme
-              (make-repeat
-               :lexeme (make-guard
-                        :lexeme (make-item)
-                        :class (make-character-class
-                                (char-range #\a #\z)))
+    (is (= 3 (run-pattern
+              (repeat
+               (guard (item) (make-character-class (char-range #\a #\z)))
                :lower 2
                :upper 4)
               inp)))
     (is (char= #\a (peek-char nil inp nil)))
 
-    (is (= 0 (run-lexeme
-              (make-repeat
-               :lexeme (make-guard
-                        :lexeme (make-item)
-                        :class (make-character-class
-                                (char-range #\a #\z)))
+    (is (= 0 (run-pattern
+              (repeat (guard (item) (make-character-class (char-range #\a #\z)))
                :lower 4
                :upper 5)
               inp)))
@@ -97,70 +74,104 @@
 
 (test alt
   (with-input-from-string (inp "abc123")
-      (let* ((letter (make-guard
-                      :lexeme (make-item)
-                      :class (make-character-class
-                              (char-range #\A #\Z)
-                              (char-range #\a #\z))))
-             (digit (make-guard
-                     :lexeme (make-item)
-                     :class (make-character-class (char-range #\0 #\9))))
-             (alnum (make-guard
-                     :lexeme (make-item)
-                     :class (make-character-class
-                             (char-range #\0 #\9)
-                             (char-range #\A #\Z)
-                             (char-range #\a #\z))))
-             (many-letters (make-repeat :lexeme letter))
-             (many-digits (make-repeat :lexeme digit))
-             (many-alnum (make-repeat :lexeme alnum)))
+    (let* ((letter (guard
+                    (item)
+                    (make-character-class
+                     (char-range #\A #\Z)
+                     (char-range #\a #\z))))
+           (digit (guard
+                   (item)
+                   (make-character-class
+                    (char-range #\0 #\9))))
+           (alnum (guard
+                   (item)
+                   (make-character-class
+                    (char-range #\0 #\9)
+                    (char-range #\A #\Z)
+                    (char-range #\a #\z))))
+           (many-letters (repeat letter))
+           (many-digits (repeat digit))
+           (many-alnum (repeat alnum)))
 
-        (is (= 3 (run-lexeme (make-alt :first many-letters :second  many-digits) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 3 (run-pattern (alt many-letters many-digits) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 3 (run-lexeme (make-alt :first many-digits :second  many-letters) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 3 (run-pattern (alt many-digits many-letters) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 6 (run-lexeme (make-alt :first many-letters :second  many-alnum) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 6 (run-pattern (alt many-letters many-alnum) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 6 (run-lexeme (make-alt :first many-alnum :second  many-letters) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 6 (run-pattern (alt many-alnum many-letters) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 0 (run-lexeme (make-alt :first many-digits :second  many-digits) inp)))
-        (is (char= #\a (peek-char nil inp nil))))))
+      (is (= 0 (run-pattern (alt many-digits many-digits) inp)))
+      (is (char= #\a (peek-char nil inp nil))))))
+
+
 
 (test cut
   (with-input-from-string (inp "abc123")
-      (let* ((letter (make-guard
-                      :lexeme (make-item)
-                      :class (make-character-class
-                              (char-range #\A #\Z)
-                              (char-range #\a #\z))))
-             (digit (make-guard
-                     :lexeme (make-item)
-                     :class (make-character-class (char-range #\0 #\9))))
-             (alnum (make-guard
-                     :lexeme (make-item)
-                     :class (make-character-class
-                             (char-range #\0 #\9)
-                             (char-range #\A #\Z)
-                             (char-range #\a #\z))))
-             (many-letters (make-repeat :lexeme letter))
-             (many-digits (make-repeat :lexeme digit))
-             (many-alnum (make-repeat :lexeme alnum)))
+    (let* ((letter (guard
+                    (item)
+                    (make-character-class
+                     (char-range #\A #\Z)
+                     (char-range #\a #\z))))
+           (digit (guard
+                   (item)
+                   (make-character-class
+                    (char-range #\0 #\9))))
+           (alnum (guard
+                   (item)
+                   (make-character-class
+                    (char-range #\0 #\9)
+                    (char-range #\A #\Z)
+                    (char-range #\a #\z))))
+           (many-letters (repeat letter))
+           (many-digits (repeat digit))
+           (many-alnum (repeat alnum)))
 
-        (is (= 3 (run-lexeme (make-cut :first many-letters :second  many-digits) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 3 (run-pattern (cut many-letters many-digits) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 3 (run-lexeme (make-cut :first many-digits :second  many-letters) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 3 (run-pattern (cut many-digits many-letters) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 3 (run-lexeme (make-cut :first many-letters :second  many-alnum) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 3 (run-pattern (cut many-letters many-alnum) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 6 (run-lexeme (make-cut :first many-alnum :second  many-letters) inp)))
-        (is (char= #\a (peek-char nil inp nil)))
+      (is (= 6 (run-pattern (cut many-alnum many-letters) inp)))
+      (is (char= #\a (peek-char nil inp nil)))
 
-        (is (= 0 (run-lexeme (make-cut :first many-digits :second  many-digits) inp)))
-        (is (char= #\a (peek-char nil inp nil))))))
+      (is (= 0 (run-pattern (cut many-digits many-digits) inp)))
+      (is (char= #\a (peek-char nil inp nil))))))
+
+(test seq
+  (with-input-from-string (inp "abc123")
+    (let* ((letter (guard
+                    (item)
+                    (make-character-class
+                     (char-range #\A #\Z)
+                     (char-range #\a #\z))))
+           (digit (guard
+                   (item)
+                   (make-character-class
+                    (char-range #\0 #\9))))
+           (alnum (guard
+                   (item)
+                   (make-character-class
+                    (char-range #\0 #\9)
+                    (char-range #\A #\Z)
+                    (char-range #\a #\z)))))
+
+      (is (= 4 (run-pattern (seq letter letter letter digit) inp)))
+      (is (= 4 (run-pattern (seq (repeat letter) digit) inp)))
+
+      ;; FIXME: The following should pass, but fails because, as currently implemented,
+      ;; there is no way to distinguish between failures and matches matching 0 items.
+      ;; Here, `(REPEAT DIGIT)' should match zero digits, and the sequence should carry
+      ;; on to match `(REPEAT LETTER)'. The solution is to modify the code to return
+      ;; `NIL' on failure instead of zero, that they may be distinguished.
+      (is (= 3 (run-pattern (seq (repeat digit) (repeat letter)) inp)))
+
+      #||#)))
